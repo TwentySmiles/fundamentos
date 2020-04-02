f22550c1dd02b1a93059cbcea58e6ccb5c4c41c0
coconut13 <hacevedo1729@gmail.com>
2020-04-02 18:31:28 -0500

    Se agrega programa que calcula el valor total de una deuda

---
+++ totalPrestamo.py
@@ -0,0 +1,9 @@
+#Asignación de variables
+A = 3000000 #Cantidad de dinero prestado
+I = 0.025 #Interes
+N = 4 #número de años
+
+#Resolviendo el ejercicio.
+Z = A * (I + 1) ** N
+
+print("El valor a pagar es: ", round(Z))
