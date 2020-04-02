db568dd12ba758918429475f0a2e3623ee8ca1ff
coconut13 <hacevedo1729@gmail.com>
2020-04-02 17:58:48 -0500

    Commit inicial

---
+++ areaCuadrado.py
@@ -0,0 +1,8 @@
+#Variable lado
+lado = 4
+
+#Calculo el área
+area = lado * lado
+
+#Mostrar en pantalla el área del cuadrado
+print(area)
---
+++ clase01Abril.py
@@ -0,0 +1,6 @@
+x = 5
+
+if x % 2 == 0:
+    print("El número", x, "es par")
+else:
+    print("El número", x, "no es par")
